25b628e238cbd030a7fa53848714cb36294912ff
Malin Ramkull <malin.ramkull@hotmail.com>
2024-04-03 16:49:51 +0200

    Update thresholds and comments on state machine

--- MALISA_Python/state_machine.py
+++ MALISA_Python/state_machine.py
@@ -8,12 +8,12 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # Thresholds
-threshold_seat = 3600
+THRESHOLD_SEAT = 3600
 THRESHOLD_HEEL = 4000
-y_dis_low = 5
-THRESHOLD_DOUBLE_STANCE = 20
-y_start_max = 60
-
+THRESHOLD_DOUBLE_STANCE = 20 # Based on the average step length for women
+WALKWAY_END_Y = 20
+WALKWAY_START_Y = 140
+WALKWAY_MID_X = 14
 
 def load_files(floor1_file_path, floor2_file_path, seat_file_path):
     floor1_df = pd.read_csv(floor1_file_path)
@@ -25,16 +25,20 @@ def load_files(floor1_file_path, floor2_file_path, seat_file_path):
     seat_array = seat_df.iloc[:, 1:].to_numpy().reshape(len(seat_df), 20, 20)
 
     # Clear data of values below threshold 
-    floor1_array[floor1_array < 500] = 0
-    floor2_array[floor2_array < 500] = 0
-    seat_array[seat_array < 100] = 0
+    floor1_array[floor1_array < 300] = 0
+    floor2_array[floor2_array < 300] = 0
+    seat_array[seat_array < 200] = 0
 
-    # special handling of seat. Was this one places in the wrong direction?
+    # Special handling of seat for visualization purposes.
     seat_array = np.rot90(seat_array, 2, (1,2))
-    # special handling of floor2. Placed in opposite direction
+
+    # Rotate floor2 (the second floor mat is placed in opposite direction) 
     floor2_array = np.rot90(floor2_array, 2, (1,2))
+
+    # Connect the separate floor mats to create the full walkway.
     floor_array = np.concatenate((floor2_array, floor1_array), axis=1)
 
+    # Save one dataframe to keep the timestamps.
     timestamp_list = floor1_df
 
     return timestamp_list, floor_array, seat_array
@@ -42,57 +46,62 @@ def load_files(floor1_file_path, floor2_file_path, seat_file_path):
 def on_prep(metrics, filepath_TED):
     seat_total_pressure = metrics['seat_total_pressure']
     
-    if(seat_total_pressure < threshold_seat):
-        # If total pressure on seat is below threshold T_sit_to_stand then log event stand
+    # Check if person is still sitting based on the total pressure on the seat
+    if(seat_total_pressure < THRESHOLD_SEAT):
+        # Log event and transition to standing state
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.start, None, None, None, None)
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.stand, None, metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.stand
+    # Still sitting, remain in preparation state
     else:
         return Tug_State.prep
     
 def on_stand(metrics, filepath_TED):
     y_distance = metrics['y_distance']
 
+    # Check if the person has started walking based on the distance in y-axel between the first and last pressure value above 0 on the walkway
     if (y_distance > THRESHOLD_DOUBLE_STANCE):
-        # If current total pressure is reaching max pressure threshold, 
-        # then logg walk because it indicates first valid heel after standing position
+        # If the y-distance exceeds the threshold, log that the first walk has started and transition to the initial walking state
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.walk1, None,metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.walk1
     else:
-        # Else logg that we are still in stand state
+        # Still standing, remain in standing state
         return Tug_State.stand
 
 def on_walk_1(metrics, filepath_TED):
-    y_first_coord = metrics['y_first_coord']
     y_last_coord = metrics['y_last_coord']
+    y_first_coord = metrics['y_first_coord']
     x_max_pressure = metrics['x_coord_max_pressure']
-    y_max_pressure = metrics['y_coord_max_pressure']
     max_pressure = metrics['max_pressure']
     left_side_total_pressure = metrics['left_side_total_pressure']
     right_side_total_pressure = metrics['right_side_total_pressure'] 
     y_distance = metrics['y_distance']
+    cop_y = metrics['cop_y']
 
-    # The first walk during TUG (walking away from the chair)
-    if (y_last_coord < 20):
+    # Check if the person has reached the end of the walkway based on the y-coordinate of the 
+    if (y_last_coord < WALKWAY_END_Y or cop_y < WALKWAY_END_Y):
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.turn1, None, metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.turn1
     else:
+        #Still in the first walk state
         event = None
         placement = None
         
+        # Check if the current gait-event is a heel by looking at the maximum pressure value on the walkway
         if(max_pressure > THRESHOLD_HEEL): 
             event = Tug_Event.heel
-            if(x_max_pressure < 14):
+            if(x_max_pressure < WALKWAY_MID_X):
                 placement = Placement.right
             else:
                 placement = Placement.left
 
+        # Check if the current gait-event is a double stance (can be either a heel or toe) based on the distance in y-axel between the first and last pressure value above 0 on the walkway 
         elif(y_distance > THRESHOLD_DOUBLE_STANCE):
             event = Tug_Event.double_stance
 
+        # Log as foot event and check the placement of the foot (left or right) by comparing the total pressure distribution of each side of the walkway in the x-axel
         else:
             event = Tug_Event.foot
-
             if(right_side_total_pressure > left_side_total_pressure):
                 placement = Placement.right
             else:
@@ -110,8 +119,9 @@ def on_walk_2(metrics, filepath_TED):
     left_side_total_pressure = metrics['left_side_total_pressure']
     right_side_total_pressure = metrics['right_side_total_pressure'] 
     y_distance = metrics['y_distance']
+    cop_y = metrics['cop_y']
 
-    if(y_first_coord > 140):
+    if(y_first_coord > WALKWAY_START_Y or cop_y > WALKWAY_START_Y):
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.turn2, None ,metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.turn2
     else:
@@ -120,16 +130,18 @@ def on_walk_2(metrics, filepath_TED):
 
         if(max_pressure > THRESHOLD_HEEL): # Special case for the begining of the test and turn 1
             event = Tug_Event.heel
-            if(x_max_pressure > 14):
+            if(x_max_pressure > WALKWAY_MID_X):
                 placement = Placement.right
             else:
                 placement = Placement.left
         
         elif(y_distance > THRESHOLD_DOUBLE_STANCE):
             event = Tug_Event.double_stance
-        
+
+        # Log as foot event and check the placement of the foot (left or right) by comparing the total pressure distribution of each side of the walkway in the x-axel
         else:
             event = Tug_Event.foot
+            # If the right side of the walkway has higher total pressure value, indicates a left foot (the person has turned, therefore right and left foot is now on opposite side of the walkway right and left side)
             if(right_side_total_pressure > left_side_total_pressure):
                 placement = Placement.left
             else:
@@ -137,11 +149,11 @@ def on_walk_2(metrics, filepath_TED):
         write_to_csv(filepath_TED, metrics['timestamp'], event, placement, metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.walk2
 
+# First turn during TUG (turning at the end of the walkway to walk back to the chair)
 def on_turn_1(metrics, filepath_TED):
-    y_distance = metrics['y_distance']
+    cop_y = metrics['cop_y']
 
-    # First turn during TUG (turning at the end of the walkway to walk back to the chair)
-    if(y_distance > 20):
+    if(cop_y > WALKWAY_END_Y):
         # Indicates that we started walking again after turning
         write_to_csv(filepath_TED, metrics['timestamp'], Tug_Event.walk2, None, metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.walk2
@@ -149,16 +161,17 @@ def on_turn_1(metrics, filepath_TED):
         # Still turning 
         return Tug_State.turn1
 
+# Second turn during TUG (turning to sit back down)
 def on_turn_2(metrics, filepath_TED):
     pressure_on_seat = metrics['seat_total_pressure']
 
-    # Second turn during TUG (turning to sit back down)
-    if(pressure_on_seat > threshold_seat):
+    # Check pressure on seat mat 
+    if(pressure_on_seat > THRESHOLD_SEAT):
         # Indicates that we are done turning and now sitting
         write_to_csv(filepath_TED, metrics['timestamp'],Tug_Event.sit, None, metrics['cop_x'], metrics['cop_y'], metrics['total_pressure'])
         return Tug_State.sit
+    # Still turning 
     else:
-        # Still turning 
         return Tug_State.turn2
 
 def on_sit(metrics, filepath_TED):
@@ -185,6 +198,7 @@ def run_analysis(file_path_mat1, file_path_mat2, file_path_seat, filepath_TED):
 
     timestamp_list, floor_array, seat_array = load_files(file_path_mat1, file_path_mat2, file_path_seat)
 
+    
     while index < len(timestamp_list):
         floor_frame = floor_array[index, :, :]
         seat_frame = seat_array[index, :, :] 
@@ -246,7 +260,7 @@ def get_file_paths(test):
     return file_path_mat1, file_path_mat2, file_path_seat 
 
 def create_heatmaps_and_plot(floor_frame, seat_frame):
-    # Create subplots for both floor mats and seat mat
+    # Create subplots for the walkway and seat 
     fig = make_subplots(rows=2, cols=1, row_heights=[150, 1200])
 
     # 12 bits gives a resolution of 4096 values, including 0 -> 4095
