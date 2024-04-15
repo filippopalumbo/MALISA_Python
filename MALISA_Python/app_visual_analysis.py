463c49a44332d79816d6daed75c6d2c3e4bad6a8
Heddaer <100926701+Heddaer@users.noreply.github.com>
2024-04-16 00:01:35 +0200

    Update app_visual_analysis.py

--- MALISA_Python/app_visual_analysis.py
+++ MALISA_Python/app_visual_analysis.py
@@ -12,12 +12,6 @@ and visualizing the results, the script enables users to gain insights into the 
 movement patterns and pressure distribution during the test.
 
 Usage:
-1. Select a TUG test from the dropdown menu.
-2. The script will load sensor data from corresponding CSV files for floor mats and the seating mat.
-3. Use the slider to choose a specific frame or timestamp from the loaded data.
-4. Metrics related to pressure distribution and center of pressure will be calculated and displayed in a table.
-5. Heatmaps showing pressure distribution on floor mats and the seating mat will be generated and visualized.
-6. Users can analyze the pressure distribution and movement patterns based on the visualizations and metrics provided.
 
 Author: [Malin Ramkull & Hedda Eriksson]
 Date: [10 april 2024]
@@ -31,6 +25,21 @@ import streamlit as st
 import plotly.graph_objects as go  
 from plotly.subplots import make_subplots
 
+def file_selector_sensor_data_floor_1(folder_path='./MALISA_Python/processed_data'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('File Fitness Mat 1', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+def file_selector_sensor_data_floor_2(folder_path='./MALISA_Python/processed_data'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('File Fitness Mat 2', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+def file_selector_sensor_data_seat(folder_path='./MALISA_Python/processed_data'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('File Seat Mat', filenames)
+    return os.path.join(folder_path, selected_filename)
+
 def load_files(floor1_file_path, floor2_file_path, seat_file_path):
     floor1_df = pd.read_csv(floor1_file_path)
     floor2_df = pd.read_csv(floor2_file_path)
@@ -41,99 +50,107 @@ def load_files(floor1_file_path, floor2_file_path, seat_file_path):
     seat_array = seat_df.iloc[:, 1:].to_numpy().reshape(len(seat_df), 20, 20)
 
     # Clear data of values below threshold 
-    floor1_array[floor1_array < 400] = 0
-    floor2_array[floor2_array < 400] = 0
-    seat_array[seat_array < 100] = 0
+    floor1_array[floor1_array < 300] = 0
+    floor2_array[floor2_array < 300] = 0
+    seat_array[seat_array < 200] = 0
 
-    # Keep one df as a "timestamp list" - to retrieve timestamp on correct frame-index
+    # Special handling of seat for visualization purposes.
+    seat_array = np.rot90(seat_array, 2, (1,2))
+
+    # Rotate floor2 (the second floor mat is placed in opposite direction) 
+    floor2_array = np.rot90(floor2_array, 2, (1,2))
+
+    # Connect the separate floor mats to create the full walkway.
+    floor_array = np.concatenate((floor2_array, floor1_array), axis=1)
+
+    # Save one dataframe to keep the timestamps.
     timestamp_list = floor1_df
 
-    return timestamp_list, floor1_array, floor2_array, seat_array
-
-def get_file_paths(test):
-
-    if test == 'test 1':
-        file_path_mat1 = 'MALISA_Python/data/tug1_mat1.csv'
-        file_path_mat2 = 'MALISA_Python/data/tug1_mat2.csv'
-        file_path_seat = 'MALISA_Python/data/tug1_seat.csv'
-    if test == 'test 2':
-        file_path_mat1 = 'MALISA_Python/data/tug2_mat1.csv'
-        file_path_mat2 = 'MALISA_Python/data/tug2_mat2.csv'
-        file_path_seat = 'MALISA_Python/data/tug2_seat.csv'
-    if test == 'test 3':
-        file_path_mat1 = 'MALISA_Python/data/tug3_mat1.csv'
-        file_path_mat2 = 'MALISA_Python/data/tug3_mat2.csv'
-        file_path_seat = 'MALISA_Python/data/tug3_seat.csv'
-    if test == 'test 4':
-        file_path_mat1 = 'MALISA_Python/data/tug4_mat1.csv'
-        file_path_mat2 = 'MALISA_Python/data/tug4_mat2.csv'
-        file_path_seat = 'MALISA_Python/data/tug4_seat.csv'  
-
-    return file_path_mat1, file_path_mat2, file_path_seat 
+    return timestamp_list, floor_array, seat_array
 
 def calculate_metrics(floor_frames, seat_frame):
     metrics = {}
     
     # Calculate and store metrics in dictionary
-    metrics['cop_x'], metrics['cop_y'], metrics['mat_nr'] = calc_cop(floor_frames)
+    metrics['cop_x'], metrics['cop_y'] = calc_cop(floor_frames)
     metrics['total_pressure'] = calc_total_pressure(floor_frames)
     metrics['seat_total_pressure'] = calc_total_pressure(seat_frame)
-    metrics['max_pressure'], metrics['x_cord_max_pressure'] = find_max_pressure(floor_frames)
-    metrics['pressure_area'] = calc_area(floor_frames)
-    metrics['y_distance'] = calc_y_distance(floor_frames)
-    
+    metrics['max_pressure'], metrics['x_coord_max_pressure'], metrics['y_coord_max_pressure'] = calc_max_pressure(floor_frames)
+    metrics['y_distance'], metrics['y_first_coord'], metrics['y_last_coord'] = calc_y_distance(floor_frames)
+    metrics['left_side_total_pressure'], metrics['right_side_total_pressure'] = calc_separate_side_total_pressure(floor_frames)
+
     return metrics
 
-def create_heatmaps_and_plot(floor1_frame, floor2_frame, seat_frame):
-    # Create subplots for both floor mats and seat mat
-    fig = make_subplots(rows=1, cols=3, column_widths=[200, 280, 280], row_heights=[800])
+def create_heatmaps_and_plot(floor_frame, seat_frame):
+    # Create subplots for the walkway and seat 
+    fig = make_subplots(rows=1, cols=2)
 
     # 12 bits gives a resolution of 4096 values, including 0 -> 4095
     fig.add_trace(go.Heatmap(z=seat_frame, zmin=0, zmax=4095, colorscale='Plasma'), row=1, col=1)
-    fig.add_trace(go.Heatmap(z=floor1_frame, zmin=0, zmax=4095, colorscale='Plasma'), row=1, col=2)
-    fig.add_trace(go.Heatmap(z=floor2_frame, zmin=0, zmax=4095, colorscale='Plasma'), row=1, col=3)
+    fig.add_trace(go.Heatmap(z=floor_frame, zmin=0, zmax=4095, colorscale='Plasma'), row=1, col=2)
 
     # Update layout
-    fig.layout.height = 800
-    fig.layout.width = (280 * 2) + 200
-    # Adjust the heights of specific rows
     fig.update_layout(
-        yaxis1=dict(domain=[0, 200/800]),  # Adjust the height of the first row
-        yaxis2=dict(domain=[0, 800/800]),  # Adjust the height of the second row
-        yaxis3=dict(domain=[0, 800/800])   # Adjust the height of the third row
+        height=1200,  # Overall height
+        width=600,  # Overall width
+        xaxis1=dict(domain=[0, 0.4]),  # First subplot width
+        xaxis2=dict(domain=[0.5, 1.0]),  # Second subplot width
+        yaxis1=dict(domain=[0.85, 1]),  # First subplot height
+        yaxis2=dict(domain=[0, 1]),  # Second subplot height
     )
-    fig.update_layout(showlegend=False)
 
     # Show the plotly chart
     st.plotly_chart(fig)
 
+
+# def plot_floor(floor_frame):
+#     fig = go.Figure(data=go.Heatmap(z=floor_frame, zmin=0, zmax=4095, colorscale='Plasma'))
+#     fig.update_layout(height=1600, width=280)  # Update layout properties
+#     st.plotly_chart(fig)
+
 def create_table(metrics):
-    info = [('timestamp', metrics['timestamp'][:21]), ('cop X', metrics['cop_x']), ('cop Y', metrics['cop_y']), ('mat nr', metrics['mat_nr']), ('seat total pressure', metrics['seat_total_pressure']), ('floor maximum pressure', metrics['max_pressure']), ('Y distance', metrics['y_distance']), ('X-cord (max pres.)', metrics['x_cord_max_pressure'])]
+    info = [('timestamp', metrics['timestamp'][:21]), 
+            ('cop X', metrics['cop_x']), 
+            ('cop Y', metrics['cop_y']), 
+            ('seat total pressure', metrics['seat_total_pressure']), 
+            ('floor total pressure', metrics['total_pressure']), 
+            ('floor maximum pressure', metrics['max_pressure']), 
+            ('X maximum pressure', metrics['x_coord_max_pressure']),
+            ('Y maximum pressure', metrics['y_coord_max_pressure']),
+            ('Y distance', metrics['y_distance']), 
+            ('Y first coord', metrics['y_first_coord']), 
+            ('Y last coord', metrics['y_last_coord']), 
+            ('left side tot pressure', metrics['left_side_total_pressure']), 
+            ('right side tot pressure', metrics['right_side_total_pressure'])]
     info = pd.DataFrame(info, columns=['Metric', 'Value'])
     st.table(info)
 
 def main():
+    st.title('TUG Visual Analysis ')
+    st.markdown('1. Ensure you have the necessary CSV files containing sensor data from the Timed Up and Go (TUG) test.')
+    st.markdown('2. Use the file selectors below to choose the sensor data files.') 
+    st.markdown('**Important:** It is crucial to pair the correct files. Otherwise, errors may occur.')
+    st.markdown('3. Adjust the slider to choose the frame (timestamp) of interest for analysis.')
 
-    test = st.selectbox(label='Select TUG test', options=['test 1', 'test 2', 'test 3', 'test 4'])
+    file_floor_1 = file_selector_sensor_data_floor_1()
+    file_floor_2 = file_selector_sensor_data_floor_2()
+    file_seat = file_selector_sensor_data_seat()
 
-    file_path_mat1, file_path_mat2, file_path_seat = get_file_paths(test)
-    
     # Load data for sensor floor mats
-    timestamp_list, floor1_array, floor2_array, seat_array = load_files(file_path_mat1, file_path_mat2, file_path_seat)
-
+    timestamp_list, floor_array, seat_array = load_files(file_floor_1, file_floor_2, file_seat)
+    
     index = st.slider('Choose frame', 0, len(timestamp_list)-1)
 
-    floor1_frame = floor1_array[index, :, :]
-    floor2_frame = floor2_array[index, :, :]
+    floor_frame = floor_array[index, :, :]
     seat_frame = seat_array[index, :, :] 
 
     # Create dictionary with metrics
-    metrics = calculate_metrics([floor1_frame, floor2_frame], seat_frame)
+    metrics = calculate_metrics(floor_frame, seat_frame)
     metrics['timestamp'] = timestamp_list.loc[index, 'timestamp']
 
     create_table(metrics)
 
-    create_heatmaps_and_plot(floor1_frame, floor2_frame, seat_frame)
+    create_heatmaps_and_plot(floor_frame, seat_frame)
 
 
 main()
